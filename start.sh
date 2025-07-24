742924eafe0f095517554c1a51fb4e3f35ad1ee0
Drew Jensen <drewrjensen@pm.me>
2025-07-24 10:49:48 -0600

    v1.21.7-5

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-exec java -Xmx4G -Xms2G -jar paper-1.21.7-32.jar nogui
+set -e
+
+# Copy jar if not already in /data
+if [ ! -f /data/paper-1.21.7-32.jar ]; then
+  cp /paper-1.21.7-32.jar /data/
+fi
+
+cd /data
+exec java -jar paper-1.21.7-32.jar
