f8d0657dd71d604bb99317df11aff4b8d7322e5b
Drew Jensen <drewrjensen@pm.me>
2025-07-24 10:53:28 -0600

    v1.21.7-6

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 set -e
 
-# Copy jar if not already in /data
-if [ ! -f /data/paper-1.21.7-32.jar ]; then
-  cp /paper-1.21.7-32.jar /data/
-fi
-
-cd /data
+cd /srv/papermc
 exec java -jar paper-1.21.7-32.jar
